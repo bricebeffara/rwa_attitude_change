0f162f51d086c6a110cb21fbf4a5a5b5305b2481
bricebeffara <brice.beffara@slowpen.science>
2018-09-01 21:53:27 +0200

    analysis direct xp06

--- xp06/02b_main_brms_expl_xp06.R
+++ xp06/02b_main_brms_expl_xp06.R
@@ -117,7 +117,7 @@ equi_gen_direct__xp06_0.5$ROPE <- "-0.5, 0.5"
 
 equi_gen_direct__xp06 <- rbind(equi_gen_direct__xp06_rwa, equi_gen_direct__xp06_0.5)
 equi_gen_direct__xp06[,c(3:5)] <- round(equi_gen_direct__xp06[,c(3:5)], 2)
-ordrow <- c("5","6","1","7","2","3","4")
+ordrow <- c(5,6,1,7,2,3,4)
 equi_gen_direct__xp06 <- equi_gen_direct__xp06[ordrow,]
 
 # export test
